beafc0c2b212c94404166833c21e109f25095a60
Gutti Torvadal <gutti.torvadal@gmail.com>
2023-11-25 23:09:09 +0100

    Add itumulator update - fixes reapper animation

--- src/itumulator/world/NonBlocking.java
+++
@@ -1,9 +0,0 @@
-package itumulator.world;
-
-/**
- * By implementing this interface, other actors / objects within the world may exist on top of it.
- * However, two objects implementing {@link NonBlocking} cannot exist on top of each other.
- */
-public interface NonBlocking {
-    
-}
