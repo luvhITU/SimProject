7a0176743d3685ef2ea7870a9a61f9e42cf6010e
luvhITU <luvh@itu.dk>
2023-12-07 15:03:17 +0100

    Even more more documentation

--- src/Places/RabbitBurrow.java
+++ src/Places/RabbitBurrow.java
@@ -7,10 +7,17 @@ import itumulator.world.NonBlocking;
 import java.awt.*;
 
 public class RabbitBurrow extends Home implements NonBlocking, DynamicDisplayInformationProvider, Hole {
+    /***
+     * Uses super of Home where maxAllowed is set to 5
+     */
     public RabbitBurrow() {
         super(5);
     }
 
+    /***
+     * See Super
+     * @return  DisplayInformation
+     */
     @Override
     public DisplayInformation getInformation() {
         return new DisplayInformation(Color.magenta, "hole-small");
