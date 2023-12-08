20fc43fc8c8abc0f8e01c08629499a533281512a
luvhITU <luvh@itu.dk>
2023-12-08 07:43:30 +0100

    More documentation

--- src/homes/Home.java
+++ src/homes/Home.java
@@ -15,6 +15,12 @@ public class Home implements DynamicDisplayInformationProvider {
     private final Set<Animal> occupants;
     private final String allowedSpecies;
 
+    /***
+     * Initializes the location, maxOccupants and allowedSpecies.
+     * @param location          Location
+     * @param maxOccupants      int
+     * @param allowedSpecies    String of the desired species
+     */
     public Home(Location location, int maxOccupants, String allowedSpecies) {
         this.location = location;
         this.maxOccupants = maxOccupants;
@@ -22,29 +28,59 @@ public class Home implements DynamicDisplayInformationProvider {
         occupants = new HashSet<>();
     }
 
+    /***
+     * See super
+     * @return  DisplayInformation
+     */
     @Override
     public DisplayInformation getInformation() {
         return new DisplayInformation(Color.cyan);
     }
 
+    /***
+     * Returns string of the allowed species in the Home
+     * @return  String of species
+     */
     public String getAllowedSpecies() {
         return allowedSpecies;
     }
 
+    /***
+     * Getter for the location of the Home
+     * @return  Location
+     */
     public Location getLocation() {
         return location;
     }
+
+    /***
+     * Getter for the int of limit of occupants in the Home
+     * @return  int
+     */
     public int getMaxOccupants() { return maxOccupants; }
+
+    /***
+     * Add the input animal to the occupants list and throws an exception if there is no room left
+     * @param animal
+     */
     public void add(Animal animal) {
         if (isFull()) { throw new IllegalStateException("Home is filled to brim already!"); }
         occupants.add(animal);
     }
 
+    /***
+     * Removes input animal from occupants list and throws an exception if the animal doesn't live there
+     * @param animal
+     */
     public void remove(Animal animal) {
         if (!occupants.contains(animal)) { throw new IllegalArgumentException(animal + " does not live here"); }
         occupants.remove(animal);
     }
 
+    /***
+     * Returns true if occupants is at max size
+     * @return  Boolean
+     */
     public boolean isFull() {
         return occupants.size() == maxOccupants;
     }
